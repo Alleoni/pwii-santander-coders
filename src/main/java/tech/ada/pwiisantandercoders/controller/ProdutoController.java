cc259175085dd9d843aac69951debd4e352263eb
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-10-16 20:56:26 -0300

    Adicionando o método FindById do CRUD (GET)

--- src/main/java/tech/ada/pwiisantandercoders/controller/ProdutoController.java
+++ src/main/java/tech/ada/pwiisantandercoders/controller/ProdutoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import tech.ada.pwiisantandercoders.model.Produto;
 import tech.ada.pwiisantandercoders.service.ProdutoService;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
@@ -21,6 +23,11 @@ public class ProdutoController {
     //@PostMapping(value = "/criar")
     public Produto criar(@RequestBody Produto produto) {
         return this.produtoService.criar(produto);
+    }
+
+    @RequestMapping (value = "/{id}", method = RequestMethod.GET)
+    public Optional<Produto> findById(@PathVariable Long id) {
+        return produtoService.buscarPorId(id);
     }
 
     //Listar todos
